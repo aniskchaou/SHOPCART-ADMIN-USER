f1e792b91cf4e75f2fef4268f23ca76f061c41fe
anis kchaou <aniskchaou@gmail.com>
2020-07-02 16:26:08 +0200

    hibernate setup

---
+++ WebContent/inex.jsp
@@ -0,0 +1,13 @@
+<%@page import="uha.anis.fr.helper.FactoryProvider"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<% out.println(FactoryProvider.getSessionFactory()); %>
+</body>
+</html>
---
+++ src/uha/anis/fr/helper/FactoryProvider.java
@@ -0,0 +1,26 @@
+package uha.anis.fr.helper;
+
+
+import javax.websocket.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+
+public class FactoryProvider {
+	
+
+	public static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory()
+	{
+		try {
+			if(sessionFactory==null)
+			{
+			sessionFactory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return sessionFactory;
+	}
+}
